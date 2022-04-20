e3e43bfbcf45763b68177558568e3f1cbe27cd17
Jhampier Giron <jhampiergiron@gmail.com>
2022-04-20 01:35:46 -0500

    Initial commit and missing TODO add button see more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './modules/landing/landing.component';
+import { HeaderComponent } from './modules/landing/header/header.component';
+import { BodyComponent } from './modules/landing/body/body.component';
+import { OurBlogsComponent } from './modules/landing/our-blogs/our-blogs.component';
+import { AboutUsComponent } from './modules/landing/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    HeaderComponent,
+    BodyComponent,
+    OurBlogsComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
